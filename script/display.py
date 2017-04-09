e22348d0d69205a164ce3c4ac7212038f9e98a2e
laurenttainturier <laurent.tainturier.pro@gmail.com>
2017-04-09 18:44:34 +0200

    Ajout menus aide et envoyer

--- script/display.py
+++ script/display.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import save
+import os
+import time
 import numpy as np
 from tkinter import *
+from tkinter import ttk
 from tkinter.messagebox import *
 from tkinter.filedialog import *
+import save
 
 
 class Display(Tk):
@@ -37,14 +40,11 @@ class Display(Tk):
 
         # Placement des widgets
         self.Can.grid(padx=10, pady=10)
+
         self.configure(menu=self.BarreMenu)
 
         self.createMatrix()
-        self.bind_all('<Control-o>', self.openSudoku)
-        self.bind_all('<Control-O>', self.openSudoku)
-        self.bind_all('<Control-s>', self.saveSudoku)
-        self.bind_all('<Control-S>', self.saveSudoku)
-        self.bind_all('<Key>', self.tryToEdit)
+        self.bindAll()
 
         self.update()
         self.setInTheMiddle()
@@ -55,6 +55,17 @@ class Display(Tk):
                 self.showError(error)
 
         self.protocol("WM_DELETE_WINDOW", self.boss.closeAll)
+
+    def bindAll(self):
+        self.bind_all('<Control-e>', self.boss.sendSudoku)
+        self.bind_all('<Control-E>', self.boss.sendSudoku)
+        self.bind_all('<Control-h>', self.showAide)
+        self.bind_all('<Control-H>', self.showAide)
+        self.bind_all('<Control-o>', self.openSudoku)
+        self.bind_all('<Control-O>', self.openSudoku)
+        self.bind_all('<Control-s>', self.saveSudoku)
+        self.bind_all('<Control-S>', self.saveSudoku)
+        self.bind_all('<Key>', self.tryToEdit)
 
     def choixMethode(self, methode):
         self.boss.setMethodeResolution(methode)
@@ -67,30 +78,23 @@ class Display(Tk):
 
     def startResolution(self):
         self.startManualEdition(False)
-        self.boss.setError("sudoku_insoluble", False)
+        # self.boss.setError("sudoku_insoluble", False)
         self.boss.startResolution(self.sudoku)
-
-    def beforeUsingCamera(self):
-        if "module_camera" in self.boss.getError():
-            self.showError("module_camera")
-        elif "disponibilite_camera" in self.boss.getError():
-            self.showError("disponibilite_camera")
-        else:
-            self.boss.Camera.takePhoto()
 
     def backUp(self):
         save.saveSudoku(self.sudoku)
-        showinfo("Sudoku", "La grille a été enregistrée avec succès !")
+        self.showInfo("sudoku_save")
 
     def openSudoku(self, evt=None):
         filepath = None
-        filepath = askopenfilename(title="Ouvrir une grille", initialdir="/Sudoku-Plotter/text", filetypes=[('Text files', '.txt')])
+        filepath = askopenfilename(title="Ouvrir une grille", initialdir="/Sudoku-Plotter/sudoku",
+                                   filetypes=[('Text files', '.txt')])
         if filepath: self.sudoku = save.readSudoku(filepath)
         self.boss.setSudoku(self.sudoku)
         self.updateSudoku()
 
     def saveSudoku(self, evt=None):
-        filepath = asksaveasfilename(title="Enregistrer une grille", initialdir="/Sudoku-Plotter/text",
+        filepath = asksaveasfilename(title="Enregistrer une grille", initialdir="/Sudoku-Plotter/sudoku",
                                      initialfile="Sudoku", filetypes=[('Text files', '.txt')])
         save.saveSudoku(self.sudoku, filepath)
 
@@ -126,6 +130,13 @@ class Display(Tk):
         self.rectangle = self.Can.create_rectangle(5 + 50 * self.x, 5 + 50 * self.y, 55 + 50 * self.x,
                                                    55 + 50 * self.y, outline='red', width=0)
 
+    def eraseResolution(self):
+        for x in range(self.nb_cases):
+            for y in range(self.nb_cases):
+                if (x, y) in self.liste_position:
+                    self.sudoku[x][y] = 0
+        self.updateSudoku(self.sudoku, self.liste_position)
+
     def tryToEdit(self, evt):
         key = evt.keysym
 
@@ -139,9 +150,6 @@ class Display(Tk):
         if key == 'space':
             self.boss.stopResolution()
 
-        if key.lower() == "m":
-            self.boss.writeSudoku(self.sudoku)
-
         if key.lower() == "o":
             self.sudoku = save.readSudoku()
             self.updateSudoku(self.sudoku)
@@ -153,13 +161,6 @@ class Display(Tk):
         if key.lower() == "s":
             self.backUp()
 
-        if key.lower() == "v":
-            try:
-                sleep = float(input("sleep = "))
-                self.boss.setSpeed(sleep)
-            except ValueError:
-                print("a doit etre un nombre")
-
         if key.lower() == "x":
             self.effacerSudoku()
 
@@ -167,11 +168,7 @@ class Display(Tk):
             self.startManualEdition()
 
         if key == "BackSpace":
-            for x in range(self.nb_cases):
-                for y in range(self.nb_cases):
-                    if (x, y) in self.liste_position:
-                        self.sudoku[x][y] = 0
-            self.updateSudoku(self.sudoku, self.liste_position)
+            self.eraseResolution()
 
         if self.edition:
             if key == 'Right':
@@ -209,18 +206,15 @@ class Display(Tk):
         if sudoku is None: sudoku = self.boss.sudoku
         self.liste_position = liste_position
         self.startManualEdition(False)
-        if "sudoku_insoluble" in self.boss.getError():
-            self.showError("sudoku_insoluble")
-        else:
-            for x in range(self.nb_cases):
-                for y in range(self.nb_cases):
-                    if (x, y) in self.liste_position:
-                        self.Can.itemconfigure(self.affichage_sudoku[x][y], text=sudoku[x][y], fill='red')
-                    else:
-                        self.Can.itemconfigure(self.affichage_sudoku[x][y], text=sudoku[x][y], fill='black')
-                    if sudoku[x][y] == 0:
-                        self.Can.itemconfigure(self.affichage_sudoku[x][y], text="", fill='black')
-            self.sudoku = np.copy(sudoku)
+        for x in range(self.nb_cases):
+            for y in range(self.nb_cases):
+                if (x, y) in self.liste_position:
+                    self.Can.itemconfigure(self.affichage_sudoku[x][y], text=sudoku[x][y], fill='red')
+                else:
+                    self.Can.itemconfigure(self.affichage_sudoku[x][y], text=sudoku[x][y], fill='black')
+                if sudoku[x][y] == 0:
+                    self.Can.itemconfigure(self.affichage_sudoku[x][y], text="", fill='black')
+        self.sudoku = np.copy(sudoku)
 
     def effacerSudoku(self):
         for x in range(self.nb_cases):
@@ -229,23 +223,24 @@ class Display(Tk):
         self.sudoku = np.zeros((self.nb_cases, self.nb_cases), int)
 
     def showError(self, error):
-        if error == "module_camera":
-            showerror("Caméra", "Désolé, le module picamera n'a pas été installé correctement !")
-        elif error == "disponibilite_camera":
-            showerror("Caméra", "Désolé, la caméra n'est pas disponible !")
-        elif error == "camera_error":
-            showerror("Caméra", "Une erreur inattendue avec la camera est survenue !\n"
-                                "Veuillez réessayer ou redémarrer votre raspberry pi")
-        elif error == "sudoku_insoluble":
-            showwarning("Sudoku", "Le sudoku n'est pas résoluble")
+        if error == "sudoku_insoluble":
+            showerror("Sudoku", "Le sudoku n'est pas résoluble !")
+        elif error == "raspi_connection":
+            showerror("Connexion impossible", "Assurez-vous d'être connécté à la Rapsberry Pi !")
         else:
             showerror("Erreur", "Une erreur inattendue est survenue !")
 
-    def showAide(self):
-        showinfo("Aide", "Le menu 'Aide' n'est pas encore disponible !")
+    def showInfo(self, info):
+        if info == "sudoku_received":
+            showinfo("Sudoku", "La grille a été transmis à la Raspberry Pi avec succès !")
+        if info == "sudoku_save":
+            showinfo("Sudoku", "La grille a été enregistrée avec succès !")
+
+    def showAide(self, evt=None):
+        self.HelpMenu()
 
     def showAPropos(self):
-        showinfo("Aide", "Le menu 'A Propos' n'est pas encore disponible !")
+        self.AboutMenu()
 
     def setInTheMiddle(self):
         geo = []
@@ -261,6 +256,75 @@ class Display(Tk):
                   + "+" + str(int(0.5 * (self.winfo_screenheight() - int(geo[1]))))
         self.geometry(new_geo)
 
+    class HelpMenu(Toplevel):
+        def __init__(self):
+            Toplevel.__init__(self)
+            self.title("Aide")
+            self.text = []
+            self.url = None
+            self.label = None
+            self.focus_set()
+            self.notebook = ttk.Notebook(self)
+            self.general = Frame(self)
+            self.raccourci = Frame(self)
+            self.notebook.add(self.general, text="Général")
+            self.notebook.add(self.raccourci, text="Raccourci")
+            self.notebook.grid(padx=10, pady=10)
+            self.showGeneral()
+            self.showRaccourci()
+
+        def showGeneral(self):
+            self.text = ["Cette application OpenSource permet la résolution de grilles de sudoku. \n",
+                         "La résolution des grilles de sudoku peut se faire à l'aide des méthodes :\n",
+                         "     - inclusion ; \n     - exclusion ; \n     - backtracking ; \n     - globale.\n",
+                         "Il est également possible de choisir la vitesse de résolution. Il est ainsi \n",
+                         "possible d'opter pour une résolution en temps réel, la plus rapide possible.\n",
+                         "Il existe également une résolution en pas à pas montrant le fonctionnement\n",
+                         "de la méthode de résolution utilisée.\n",
+                         "Les grilles peuvent être enregistrées afin de pouvoir y accéder plus simple-\n",
+                         "ment, sans avoir à réécrire chacun des chiffres.\n",
+                         "La grille peut aussi être envoyée à la Raspberry Pi, qui pourra, si elle est\n",
+                         "connectée, écrire physiquement celle-ci à l'aide du bras mécanique."]
+            self.showLabel(self.general)
+
+        def showRaccourci(self):
+            self.text = ["De nombreux raccourcis claviers sont disponibles afin de faciliter\n l'utilisation",
+                         "de l'application.\n\n",
+                         " Ctrl + O \t\tOuvrir une grille existante (en format .txt)\n",
+                         " Ctrl + S \t\tSauvegarder une nouvelle grille (en format .txt)\n",
+                         " Ctrl + E \t\tEnvoyer la grille courante à la Raspberry Pi\n",
+                         " Ctrl + H \t\tOuverture du menu d'aide\n",
+                         " Entrée \t\tEditer la grille manuellement\n",
+                         " Espace \t\tSuspendre la résolution\n",
+                         " Retour \t\tAffihcer la grille avant sa résolution\n",
+                         " F5 \t\tLancer la résolution\n",
+                         " O \t\tOuverture de la dernière grille créée\n",
+                         " S \t\tSauvegarde rapide\n",
+                         " X \t\tEffacer la grille courante"]
+            self.showLabel(self.raccourci)
+
+        def showLabel(self, Frame):
+            self.label = Label(Frame)
+            self.url = Label(Frame)
+            url = "Plus d'informations disponibles sur www.github.com/alphter/Sudoku-Plotter"
+            self.label.configure(text=''.join(self.text), font=('Times', 12), borderwidth=3,
+                                 anchor=N, justify=LEFT, height=15)
+            self.url.configure(text=url, font=('Times', 12), borderwidth=3, anchor=N, justify=LEFT)
+            self.label.grid(padx=15, pady=15, sticky='w')
+            self.url.grid(padx=15, pady=5)
+
+    class AboutMenu(Toplevel):
+        def __init__(self):
+            Toplevel.__init__(self)
+            self.title("A propos")
+            self.label = Label(self)
+            self.focus_set()
+            self.text = ["Cette application a été\n développée par Laurent Tainturier\n et Alphonse Terrier\n",
+                         "dans le cadre de leur TIPE\n intitulé 'Sudoku Plotter'\n supervisé par Patrick Couvez.\n\n\n",
+                         "Plus d'informations disponibles sur :\n www.github.com/alphter/Sudoku-Plotter"]
+            self.label.configure(text=''.join(self.text), font=('Times', 12), borderwidth=3)
+            self.label.grid(padx=5, pady=10)
+
     class BarreMenu(Menu):
         """
         Hérite de la classe Menu() qui permet à l'utilisateur de définir
@@ -273,6 +337,18 @@ class Display(Tk):
             self.mode = IntVar()
             self.vitesse = IntVar()
             self.mode_resolution = IntVar()
+            os.chdir("../pictures/tkinter/")
+            self.save_icon = PhotoImage(file="save_icone.png").subsample(24)
+            self.open_icon = PhotoImage(file="open_icone.png").subsample(16)
+            self.edit_icon = PhotoImage(file="edit_icone.png").subsample(2)
+            self.delete_icon = PhotoImage(file="delete_icone.png").subsample(16)
+            self.send_icon = PhotoImage(file="send_icon.png").subsample(43)
+            self.run_icon = PhotoImage(file="run_icone.png").subsample(13)
+            self.pause_icon = PhotoImage(file="pause_icone.png").subsample(22)
+            self.stop_icon = PhotoImage(file="stop_icone.png").subsample(10)
+            self.help_icon = PhotoImage(file="help_icone.png").subsample(19)
+            self.about_icon = PhotoImage(file="about_icon.png").subsample(37)
+            os.chdir("../../script/")
 
             # Sélection par défaut des items
             self.mode_resolution.set(0)
@@ -283,29 +359,43 @@ class Display(Tk):
             self.menu_edition = Menu(self, tearoff=0)
             self.menu_resolution = Menu(self, tearoff=0)
             self.menu_methode = Menu(self, tearoff=0)
-            self.menu_vitesse = Menu(self, tearoff=0)
             self.menu_aide = Menu(self, tearoff=0)
 
             # Ajout des menus
             self.add_cascade(label="Edition", menu=self.menu_edition)
             self.add_cascade(label="Résolution", menu=self.menu_resolution)
             self.add_cascade(label="Méthode", menu=self.menu_methode)
-            self.add_cascade(label="Vitesse", menu=self.menu_vitesse)
             self.add_cascade(label="Aide", menu=self.menu_aide)
 
             # Ajout des items du menu 'Edition'
-            self.menu_edition.add_command(label="Sauvegarder", command=self.boss.saveSudoku, accelerator="Ctrl+S")
-            self.menu_edition.add_command(label="Automatique", command=self.boss.openSudoku, accelerator="Ctrl+O")
-            self.menu_edition.add_command(label="Manuelle", command=self.boss.startManualEdition)
-            self.menu_edition.add_command(label="Effacer", command=self.boss.effacerSudoku)
+            self.menu_edition.add_command(label="Ouvrir", command=self.boss.openSudoku, accelerator="Ctrl+O",
+                                          image=self.open_icon, compound=LEFT)
+            self.menu_edition.add_command(label="Sauvegarder", command=self.boss.saveSudoku, accelerator="Ctrl+S",
+                                          image=self.save_icon, compound=LEFT)
+            self.menu_edition.add_command(label="Envoyer", command=self.boss.boss.sendSudoku,
+                                          image=self.send_icon, compound=LEFT, accelerator="Ctrl+E")
+            self.menu_edition.add_command(label="Manuelle", command=self.boss.startManualEdition,
+                                          image=self.edit_icon, compound=LEFT)
+            self.menu_edition.add_command(label="Effacer", command=self.boss.effacerSudoku,
+                                          image=self.delete_icon, compound=LEFT)
 
             # Ajout des items du menu 'Résolution'
             self.menu_resolution.add_radiobutton(label="Directe", value=0, variable=self.mode,
                                                  command=lambda: self.boss.choixMode("Directe"))
-            self.menu_resolution.add_radiobutton(label="Pas à pas", value=1, variable=self.mode,
-                                                 command=lambda: self.boss.choixMode("Pas à pas"))
+            vitesse = Menu(tearoff=0)
+            self.menu_resolution.add_cascade(label="Pas à pas", menu=vitesse)
+            vitesse.add_radiobutton(label="Rapide", value=1, variable=self.mode,
+                                    command=lambda: self.boss.choixMode("Rapide"))
+            vitesse.add_radiobutton(label="Lente", value=2, variable=self.mode,
+                                    command=lambda: self.boss.choixMode("Lente"))
+
             self.menu_resolution.add_separator()
-            self.menu_resolution.add_command(label="Lancer", command=self.boss.startResolution)
+            self.menu_resolution.add_command(label="Lancer", command=self.boss.startResolution, accelerator="F5",
+                                             image=self.run_icon, compound=LEFT)
+            self.menu_resolution.add_command(label="Pause", command=self.boss.boss.pauseResolution,
+                                             image=self.pause_icon, compound=LEFT)
+            self.menu_resolution.add_command(label="Stop", command=self.boss.boss.stopResolution,
+                                             image=self.stop_icon, compound=LEFT)
 
             # Ajout des items du menu "Méthode"
             self.menu_methode.add_radiobutton(label="Globale", value=0, variable=self.mode_resolution,
@@ -317,12 +407,8 @@ class Display(Tk):
             self.menu_methode.add_radiobutton(label="Backtracking", value=3, variable=self.mode_resolution,
                                               command=lambda: self.boss.choixMethode("Backtracking"))
 
-            # Ajout des items du menu "Vitesse"
-            self.menu_vitesse.add_radiobutton(label="Rapide", value=0, variable=self.vitesse,
-                                                 command=lambda: self.boss.choixVitesse("Rapide"))
-            self.menu_vitesse.add_radiobutton(label="Lente", value=1, variable=self.vitesse,
-                                                 command=lambda: self.boss.choixVitesse("Lente"))
-
             # Ajout des items du menu "Aide
-            self.menu_aide.add_command(label="Fonctionnement", command=self.boss.showAide)
-            self.menu_aide.add_command(label="A Propos", command=self.boss.showAPropos)
+            self.menu_aide.add_command(label="A propos", command=self.boss.showAPropos,
+                                       image=self.about_icon, compound=LEFT)
+            self.menu_aide.add_command(label="Aide", command=self.boss.showAide, image=self.help_icon,
+                                       compound=LEFT)
